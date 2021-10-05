2516ca2b46f77df7ce90328ab8d75e76055a5034
JuanDavidTriana <juandavidtriana.jdt@gmail.com>
2021-10-05 07:37:30 -0500

    Creando rutas dinamicas

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import Login from "../views/Login.vue"
 import Register from "../views/Register.vue"
+import Category from "../views/Category.vue"
 
 const routes = [
   {
@@ -19,6 +20,10 @@ const routes = [
     name: 'Register',
     component: Register
   },
+  {
+    path: "/:category",
+    component: Category,
+  }
   
 ]
 
