f89a672814c1f7799989dc792d16acc97f91d5ad
JuanDavidTriana <juandavidtriana.jdt@gmail.com>
2021-10-06 13:38:08 -0500

    Pintando productos en el carrito

--- client/src/api/cart.js
+++ client/src/api/cart.js
@@ -1,48 +1,49 @@
-import {} from "lodash"
+import { uniqBy, countBy } from "lodash";
 import {API_URL, PRODUCTS} from "../utils/constants"
 
 export function addProductCartApi(idProduct) {
-    const products = getCartApi();
-    products.push(idProduct);
+  const products = getCartApi();
+  products.push(idProduct);
+
+  localStorage.setItem(PRODUCTS, JSON.stringify(products));
+}
+
+export function getCartApi() {
+    const products = localStorage.getItem(PRODUCTS);
   
-    localStorage.setItem(PRODUCTS, JSON.stringify(products));
+    if (!products) return [];
+  
+    return JSON.parse(products);
+  }
+
+  export async function getProductsCartApi() {
+    const idProducts = getCartApi();
+  
+    if (idProducts.lenght === 0) return null;
+  
+    try {
+      const products = [];
+  
+      for await (const idProduct of idProducts) {
+        const response = await fetch(`${API_URL}/products/${idProduct}`);
+        const result = await response.json();
+        products.push(result);
+      }
+  
+      const productsCount = countBy(products, (product) => {
+        return product.name;
+      });
+  
+      const combined = uniqBy(products, (product) => {
+        const productTemp = product;
+        productTemp.quantity = productsCount[product.name];
+        return productTemp.name;
+      });
+  
+      return combined;
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
   }
   
-export function getCartApi() {
-      const products = localStorage.getItem(PRODUCTS);
-    
-      if (!products) return [];
-    
-      return JSON.parse(products);
-    }
-
-    export async function getProductsCartApi() {
-      const idProducts = getCartApi();
-    
-      if (idProducts.lenght === 0) return null;
-    
-      try {
-        const products = [];
-    
-        for await (const idProduct of idProducts) {
-          const response = await fetch(`${API_URL}/products/${idProduct}`);
-          const result = await response.json();
-          products.push(result);
-        }
-    
-        const productsCount = countBy(products, (product) => {
-          return product.name;
-        });
-    
-        const combined = uniqBy(products, (product) => {
-          const productTemp = product;
-          productTemp.quantity = productsCount[product.name];
-          return productTemp.name;
-        });
-    
-        return combined;
-      } catch (error) {
-        console.log(error);
-        return null;
-      }
-    }
